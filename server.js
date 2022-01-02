a3dc8b38fc382d48adcee69bfa7307fd6409bc4f
jbryanmcgrath <90098243+jbryanmcgrath@users.noreply.github.com>
2022-01-01 20:21:30 -0500

    11.2 ankasa

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-//parse incoming string or array data
+
 app.use(express.urlencoded({ extended: true }));
-//parse incoming JSON data 
 app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
@@ -41,6 +40,7 @@ function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
 }
+
 function createNewAnimal(body, animalsArray) {
     const animal = body;
     animalsArray.push(animal);
@@ -67,8 +67,6 @@ function validateAnimal(animal) {
     return true;
 }
 
-
-
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -90,7 +88,6 @@ app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
 
-    // if any data in req.body is incorrect, send 400 error back
     if (!validateAnimal(req.body)) {
         res.status(400).send('The animal is not properly formatted.');
     } else {
@@ -98,7 +95,6 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     }
 });
-
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
